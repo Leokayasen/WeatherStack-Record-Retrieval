aa22df30ecfec651d3e810b388a942b06049998b
TheAdmiral <74490542+Leokayasen@users.noreply.github.com>
2024-03-14 13:01:33 +0000

    main.py - Source code for WeatherStack data retrieval

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+API_KEY = "38c2d5cb977fe2306231c68517b66f22"
+WS_URL = "http://api.weatherstack.com/current"
+
+City = 'London'
+parameters = {'access_key': API_KEY, 'query': City}
+response = requests.get(WS_URL, parameters)
+js = response.json()
+print(js)
+print()
+
+temperature = js['current']['temperature']
+date = js['location']['localtime']
+city = js['location']['name']
+country = js['location']['country']
+
+print(f"The temperature in {city}, {country} on {date} is {temperature} degrees Celsius")
